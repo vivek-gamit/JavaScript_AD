5a4efbed5e82281d3c50b2af3a202a9ea67ebc3a
Vivek <vgamit637@gmail.com>
2025-10-28 16:04:39 +0530

    object error fixing

--- OOP_concept/script.js
+++ OOP_concept/script.js
@@ -8,7 +8,7 @@ CreatePencil.prototype.write = function(text){
         let h1 = document.createElement("h1");
         h1.textContent = text;
         h1.style.color = this.color;
-        document.body.appendChild(h1)
+        document.body.appendChild(h1);
 
     }
 

515d4f5a866d81dba974e06cdecc3dea7e61a429
vivek-gamit <vgamit637@gmail.com>
2025-10-28 12:56:02 +0530

    prototype

--- OOP_concept/script.js
+++ OOP_concept/script.js
@@ -2,14 +2,15 @@ function CreatePancile(name,price,color){
     this.name = name;
     this.price = price;
     this.color = color;
-    this.write = function(text){
+}
+
+CreatePancile.prototype.write = function(text){
         let h1 = document.createElement("h1");
         h1.textContent = text;
-        h1.style.color = color;
+        h1.style.color = this.color;
         document.body.appendChild(h1)
 
     }
-}
 
 let pencile1 = new CreatePancile("natraj",20,"red");
 let pencile2 = new CreatePancile("Doms",30,"blue");
